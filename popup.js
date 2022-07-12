e5ce17b7cfa2a1bb2b8c42c2296a5b69575faac0
Steve <steve.c.cardozo@gmail.com>
2022-07-12 03:21:18 -0400

    highlight list items when filter button is clicked

--- popup.js
+++ popup.js
@@ -1,6 +1,19 @@
 const form = document.getElementById("form");
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
-  console.log("test");
+
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: filter
+  }); 
 });
+
+function filter() {
+  const listItems = document.querySelectorAll("#job-container li");
+
+  for (item of listItems)
+    item.style.backgroundColor = "red";
+}
